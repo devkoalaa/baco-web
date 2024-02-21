a0a362afbb7cd6efb88ec11fefd7c3d34eefee69
Herbert Valadares <herberticleber@gmail.com>
2024-02-20 23:51:58 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import "../app/globals.css";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "../lib/utils";
+import { ReactNode } from "react";
+import { ThemeProvider } from "@/components/theme-provider";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
